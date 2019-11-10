b5d4f0d8db3b8e534279f6cf641cda40393e96cb
lpalacheva <LPalacheva@sportmaster.ru>
2019-11-11 02:17:45 +0300

    refactor some methods

--- src/main/java/HumanData.java
+++ src/main/java/HumanData.java
@@ -12,48 +12,60 @@ import java.util.*;
 
 public class HumanData {
     public static void main(String[] args) throws Exception{
+        String fileRepository = "src/main/resources/";
         int rowNum = readRowNum();
 
-        ArrayList<String> names = new ArrayList<String>(),
-                patronymics = new ArrayList<String>(),
-                dates = new ArrayList<String>(),
-                ages = new ArrayList<String>(),
-                index = new ArrayList<String>(),
-                countries = new ArrayList<String>(),
-                houses = new ArrayList<String>(),
-                flats = new ArrayList<String>(),
-                sexes = new ArrayList<String>();
+        String[] names = new String[rowNum],
+                patronymics = new String[rowNum],
+                dates = new String[rowNum],
+                ages = new String[rowNum],
+                index = new String[rowNum],
+                countries = new String[rowNum],
+                houses = new String[rowNum],
+                flats = new String[rowNum],
+                sexes = new String[rowNum],
+                hometowns = new String[rowNum],
+                regions = new String[rowNum],
+                cities = new String[rowNum],
+                streets = new String[rowNum],
+                surnames = new String[rowNum];
+
         int daysInYear = 365;
         int hundredYearsInDays = 100 * daysInYear;
 
-        ArrayList<String> femaleNames = getArrayFromFile("src/main/resources/FemaleNames.txt");
-        ArrayList<String> maleNames = getArrayFromFile("src/main/resources/MaleNames.txt");
-        ArrayList<String> surnames = getArrayFromFile("src/main/resources/Surnames.txt");
-        ArrayList<String> femalePatronymic = getArrayFromFile("src/main/resources/FemalePatronymic.txt");
-        ArrayList<String> malePatronymic = getArrayFromFile("src/main/resources/MalePatronymic.txt");
-        ArrayList<String> streets = getArrayFromFile("src/main/resources/Streets.txt");
-        ArrayList<String> cities = getArrayFromFile("src/main/resources/Cities.txt");
-        ArrayList<String> regions = getArrayFromFile("src/main/resources/Regions.txt");
+        ArrayList<String> femaleNamesList = getArrayFromFile(fileRepository + "FemaleNames.txt");
+        ArrayList<String> maleNamesList = getArrayFromFile(fileRepository + "MaleNames.txt");
+        ArrayList<String> surnamesList = getArrayFromFile(fileRepository + "Surnames.txt");
+        ArrayList<String> femalePatronymicList = getArrayFromFile(fileRepository + "FemalePatronymic.txt");
+        ArrayList<String> malePatronymicList = getArrayFromFile(fileRepository + "MalePatronymic.txt");
+        ArrayList<String> streetsList = getArrayFromFile(fileRepository + "Streets.txt");
+        ArrayList<String> citiesList = getArrayFromFile(fileRepository + "Cities.txt");
+        ArrayList<String> regionsList = getArrayFromFile(fileRepository + "Regions.txt");
+        ArrayList<String> hometownsList = getArrayFromFile(fileRepository + "Cities.txt");
 
         for (int i = 0; i < rowNum; i++){
             int randomCountOfDays = randomNumber(hundredYearsInDays);
-            dates.add(i, getData(randomCountOfDays));
-            ages.add(i, getAge(randomCountOfDays, daysInYear));
-            index.add(i, Integer.toString(100000 + randomNumber(899999)));
-            countries.add(i, "Россия");
-            houses.add(i, Integer.toString(randomNumber(300)));
-            flats.add(i, Integer.toString(randomNumber(300)));
+            dates[i] = getData(randomCountOfDays);
+            ages[i] = getAge(randomCountOfDays, daysInYear);
+            index[i] = Integer.toString(100000 + randomNumber(899999));
+            countries[i] = "Россия";
+            houses[i] = Integer.toString(randomNumber(300));
+            flats[i] = Integer.toString(randomNumber(300));
             if(randomNumber(2) == 1){
-                sexes.add(i, "MУЖ");
-                names.add(i, maleNames.get(randomNumber(maleNames.size())));
-                surnames.add(i, surnames.get(randomNumber(surnames.size())));
-                patronymics.add(i, malePatronymic.get(randomNumber(malePatronymic.size())));
+                sexes[i] = "MУЖ";
+                names[i] = maleNamesList.get(randomNumber(maleNamesList.size() - 1));
+                surnames[i] = surnamesList.get(randomNumber(surnamesList.size() - 1));
+                patronymics[i] = malePatronymicList.get(randomNumber(malePatronymicList.size() - 1));
             } else {
-                sexes.add(i, "ЖЕН");
-                names.add(i, femaleNames.get(randomNumber(femaleNames.size())));
-                surnames.add(i, surnames.get(randomNumber(surnames.size())) + "а");
-                patronymics.add(i, femalePatronymic.get(randomNumber(femalePatronymic.size())));
+                sexes[i] = "ЖЕН";
+                names[i] = femaleNamesList.get(randomNumber(femaleNamesList.size() - 1));
+                surnames[i] = surnamesList.get(randomNumber(surnamesList.size() - 1)) + "а";
+                patronymics[i] = femalePatronymicList.get(randomNumber(femalePatronymicList.size()));
             }
+            hometowns = getRandomArray(hometownsList, rowNum);
+            regions = getRandomArray(regionsList, rowNum);
+            cities = getRandomArray(citiesList, rowNum);
+            streets = getRandomArray(streetsList, rowNum);
         }
 
         HSSFWorkbook workBook = new HSSFWorkbook();
@@ -64,21 +76,45 @@ public class HumanData {
             i++;
         }
 
-        fillTheColumn(sheet, rowNum, 0, names, "Имя");
-        fillTheColumn(sheet, rowNum, 1, surnames, "Фамилия");
-        fillTheColumn(sheet, rowNum, 2, patronymics, "Отчество");
-        fillTheColumn(sheet, rowNum, 3, ages, "Возраст");
-        fillTheColumn(sheet, rowNum, 4, sexes, "Пол");
-        fillTheColumn(sheet, rowNum, 5, dates, "Дата рождения");
-        fillTheColumnWithRandom(sheet, rowNum, 6, cities, "Место рождения");
-        fillTheColumn(sheet, rowNum, 7, index, "Индекс");
-        fillTheColumn(sheet, rowNum, 8, countries, "Страна");
-        fillTheColumnWithRandom(sheet, rowNum, 9, regions, "Область");
-        fillTheColumnWithRandom(sheet, rowNum, 10, cities, "Город");
-        fillTheColumnWithRandom(sheet, rowNum, 11, streets, "Улица");
-        fillTheColumn(sheet, rowNum, 12, houses, "Дом");
-        fillTheColumn(sheet, rowNum, 13, flats, "Квартира");
-        File file = new File("src/main/resources/data.xls");
+        String[] columns  = {
+                "Имя",
+                "Фамилия",
+                "Отчество",
+                "Возраст",
+                "Пол",
+                "Дата рождения",
+                "Место рождения",
+                "Индекс",
+                "Страна",
+                "Область",
+                "Город",
+                "Улица",
+                "Дом",
+                "Квартира"
+        };
+
+        String[][] allData = {
+                names,
+                surnames,
+                patronymics,
+                ages,
+                sexes,
+                dates,
+                hometowns,
+                index,
+                countries,
+                regions,
+                cities,
+                streets,
+                houses,
+                flats
+        };
+
+        for (int columnIndex = 0; columnIndex < columns.length; columnIndex++){
+            fillTheColumn(sheet, rowNum, columnIndex, allData[columnIndex], columns[columnIndex]);
+        }
+
+        File file = new File(fileRepository + "data.xls");
         FileOutputStream outFile = new FileOutputStream(file);
         workBook.write(outFile);
         System.out.println("Файл создан. Путь: " + file.getAbsolutePath());
@@ -108,7 +144,7 @@ public class HumanData {
         return (int) (Math.random() * max);
     }
 
-    private static void fillTheColumnWithRandom(HSSFSheet sheet, int rowNum, int columnNum, ArrayList<String> list, String columnName){
+    private static void fillTheColumn(HSSFSheet sheet, int rowNum, int columnNum, String[] array, String columnName){
         String cellValue;
         Cell cell;
         Row row;
@@ -117,24 +153,7 @@ public class HumanData {
         cell.setCellValue(columnName);
         int i = 1;
         while (i <= rowNum) {
-            cellValue = list.get(randomNumber(list.size()));
-            row = sheet.getRow(i);
-            cell = row.createCell(columnNum, CellType.STRING);
-            cell.setCellValue(cellValue);
-            i += 1;
-        }
-    }
-
-    private static void fillTheColumn(HSSFSheet sheet, int rowNum, int columnNum, ArrayList<String> list, String columnName){
-        String cellValue;
-        Cell cell;
-        Row row;
-        row = sheet.getRow(0);
-        cell = row.createCell(columnNum, CellType.STRING);
-        cell.setCellValue(columnName);
-        int i = 1;
-        while (i <= rowNum) {
-            cellValue = list.get(i-1);
+            cellValue = array[i-1];
             row = sheet.getRow(i);
             cell = row.createCell(columnNum, CellType.STRING);
             cell.setCellValue(cellValue);
@@ -162,5 +181,13 @@ public class HumanData {
 
     private static String getAge(int daysSinceBirth, int daysInYear){
         return Integer.toString(daysSinceBirth / daysInYear);
+    }
+
+    private static String[] getRandomArray(ArrayList<String> originArray, int newArrayLength){
+        String[] newArray = new String[newArrayLength];
+        for (int i = 0; i < newArrayLength; i ++) {
+            newArray[i] = originArray.get(randomNumber(originArray.size() - 1));
+        }
+        return newArray;
     }
 }
